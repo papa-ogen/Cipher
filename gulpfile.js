495ad48b7061a368378fb41611ac39182fe3e310
k320940 <jorgen.thelin@consult.nordea.com>
2017-08-08 09:44:18 +0200

    Added useref uglify if

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
 gulp.task('sass', function () {
   return gulp.src('app/scss/**/*.scss')
@@ -13,7 +16,8 @@ gulp.task('sass', function () {
 
 gulp.task('watch', ['browserSync', 'sass'], function () {
   gulp.watch('app/scss/**/*.scss', ['sass']);
-  // Other watchers
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 })
 
 gulp.task('browserSync', function () {
@@ -23,3 +27,9 @@ gulp.task('browserSync', function () {
     },
   })
 })
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+});
