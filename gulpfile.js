db3b367d47763b6bdcc374ba1673db93a9a7452c
k320940 <jorgen.thelin@consult.nordea.com>
2017-08-08 09:33:08 +0200

    Added watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
-});
+})
+
+gulp.task('watch', function () {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Other watchers
+})
