dade7165a957c3c96e0466911d0b451eaf6d2f05
k320940 <jorgen.thelin@consult.nordea.com>
2017-08-08 09:36:03 +0200

    Added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browserSync', 'sass'], function () {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   // Other watchers
+})
+
+gulp.task('browserSync', function () {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
