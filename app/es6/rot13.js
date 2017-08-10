bdacb135372c44557bd31e0b44bc64f3fb89f798
papa-ogen <jorgen.thelin@consult.nordea.com>
2017-08-10 10:09:16 +0200

    Ro13 refactoring

--- app/es6/rot13.js
+++ app/es6/rot13.js
@@ -21,8 +21,6 @@
       this.DEFAULT_NRCIPHER = 5;
       this.inputEl = this.GLOBAL_INPUT || wrapper.querySelector('.rot-13-input');
       this.outputEl = wrapper.querySelector('.rot-13-output');
-      this.cipherVal = 0;
-      this.nrCipherVal = 0;
 
       if (this.inputEl.value.length > 0) {
         this.update();
@@ -31,47 +29,32 @@
       this.inputEl.addEventListener('input', e => this.update(e));
     }
 
-    createCipherIndex(index, cipherVal, arrayLength) {
-      var cipherIndex = 0;
-      if (cipherVal < 0) return (parseInt(index) + parseInt(cipherVal)) + arrayLength;
-
-      cipherIndex = (parseInt(index) + parseInt(cipherVal)) - arrayLength;
-
-      if (cipherIndex < 0) cipherIndex = cipherIndex + arrayLength;
-
-      return cipherIndex;
+    getLetterIndex(letter) {
+      return this.ALPHABET.indexOf(letter.toUpperCase());
     }
 
     decipher(input) {
-      var output = "";
-      var cipherIndex = 0;
+      return input.split('').map((letter) => {
+        const letterIndex = this.getLetterIndex(letter);
+        const cipher = this.DEFAULT_CIPHER;
+        const alength = this.ALPHALENGTH;
 
-      input = input.toUpperCase();
+        return letterIndex + cipher >= alength ? this.ALPHABET[(letterIndex + cipher - alength)] : this.ALPHABET[letterIndex + cipher];
+      });
+    }
 
-      for (var i = 0; i < input.length; i++) {
-        if (input[i].match(/\d/)) {
-          cipherIndex = this.createCipherIndex(input[i], this.nrCipherVal, 10);
-          output += cipherIndex;
-        } else {
-          var index = this.ALPHABET.indexOf(input[i]);
+    decipherNumber(input) {
+      const cipher = this.DEFAULT_NRCIPHER;
 
-          if (index !== -1) {
-            cipherIndex = this.createCipherIndex(index, this.cipherVal, this.ALPHALENGTH);
-            output += this.ALPHABET[cipherIndex];
-          } else {
-            output += input[i];
-          }
-        }
-      }
-
-      return output;
+      return input.split('').map((number) => { return number + cipher; });
     }
 
     update() {
-      let input = this.inputEl.value.trim();
-      let output = this.decipher(input).trim();
-
-      this.outputEl.value = output;
+      const reg = /^\d+$/;
+      const input = this.inputEl.value.trim();
+      const output = reg.test(input) ? this.decipherNumber(input) : this.decipher(input);
+      
+      this.outputEl.value = output.join('');
     }
   }
 
@@ -83,117 +66,3 @@
   }
 
 })(cipherHelper); // eslint-disable-line no-undef
-
-// (function ($, cipherHelper) {
-//   "use strict";
-
-//   var c = cipherHelper;
-//   var ALPHABET = c.alphabet;
-//   var ALPHALENGTH = c.alphabetLength();
-//   var DEFAULT_CIPHER = 13;
-//   var DEFAULT_NRCIPHER = 5;
-//   var GLOBAL_INPUT = $(c.GLOBAL_INPUT);
-
-//   var Rot13 = function (obj) {
-//     // If Constructor is called without "new" for instance from a call(), re-cell the constructor with this as obj argument.
-//     if (!(this instanceof Rot13)) {
-//       return new Rot13(this);
-//     }
-//     // Set the instance to make the instance available in all methods and events.
-//     var rot13 = $.extend(this, {
-//       obj: $(obj),
-//       InputEl: $(".rot-13-input", obj), // mandatory
-//       OutputEl: $(".rot-13-output", obj), // mandatory
-//       alphaSliderEl: $(".rot-13-slider-container-a", obj),
-//       alphaSliderVal: $(".rot-13-slider-a", obj),
-//       numberSliderEl: $(".rot-13-slider-container-n", obj),
-//       numberSliderVal: $(".rot-13-slider-n", obj),
-//       cipherVal: 0,
-//       nrCipherVal: 0
-//     });
-//     return this.init(obj);
-//   };
-
-//   Rot13.prototype = {
-//     createCipherIndex: function (index, cipherVal, arrayLength) {
-//       var cipherIndex = 0;
-//       if (cipherVal < 0) return (parseInt(index) + parseInt(cipherVal)) + arrayLength;
-
-//       cipherIndex = (parseInt(index) + parseInt(cipherVal)) - arrayLength;
-
-//       if (cipherIndex < 0) cipherIndex = cipherIndex + arrayLength;
-
-//       return cipherIndex;
-//     },
-//     deCipher: function (input) {
-//       var output = "";
-//       var cipherIndex = 0;
-
-//       input = input.toUpperCase();
-
-//       for (var i = 0; i < input.length; i++) {
-//         if (input[i].match(/\d/)) {
-//           cipherIndex = this.createCipherIndex(input[i], this.nrCipherVal, 10);
-//           output += cipherIndex;
-//         } else {
-//           var index = ALPHABET.indexOf(input[i]);
-
-//           if (index !== -1) {
-//             cipherIndex = this.createCipherIndex(index, this.cipherVal, ALPHALENGTH);
-//             output += ALPHABET[cipherIndex];
-//           } else {
-//             output += input[i];
-//           }
-//         }
-//       }
-
-//       return output;
-//     },
-//     updatePanels: function () {
-//       var input = $.trim(this.InputEl.val());
-//       var output = $.trim(this.deCipher(input));
-
-//       this.OutputEl.val(output);
-//     },
-//     init: function () {
-//       var _this = this;
-
-//       this.cipherVal = this.alphaSliderVal.val() || DEFAULT_CIPHER;
-//       this.nrCipherVal = this.numberSliderVal.val() || DEFAULT_NRCIPHER;
-//       this.InputEl = GLOBAL_INPUT.length > 0 ? GLOBAL_INPUT : this.InputEl;
-
-//       if (this.InputEl.val().length > 0) {
-//         this.updatePanels();
-//       }
-
-//       this.InputEl.bind('input propertychange', function () {
-//         _this.updatePanels();
-//       });
-
-//       // If elements exist attach to event
-//       if (this.alphaSliderEl.length > 0) {
-//         this.alphaSliderEl.on("slidestop", function (event, ui) {
-//           _this.cipherVal = _this.alphaSliderVal.val();
-
-//           _this.updatePanels();
-//         });
-//       }
-
-//       if (this.numberSliderEl.length > 0) {
-//         this.numberSliderEl.on("slidestop", function (event, ui) {
-//           _this.nrCipherVal = _this.numberSliderVal.val();
-
-//           _this.updatePanels();
-//         });
-//       }
-//     }
-//   };
-
-//   $('.js-rot-13').each(Rot13);
-
-//   return Rot13;
-// })(jQuery, cipherHelper);
-
-// /* Todo:
-// 	negative number shift
-// */
